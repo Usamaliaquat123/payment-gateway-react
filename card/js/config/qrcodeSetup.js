dd285743185a1ec08bc1af6c89cc5d24c2607aa0
Usamaliaquat123 <inconnent12345@outlook.com>
2020-06-22 14:01:43 +0500

    ad

--- card/js/config/qrcodeSetup.js
+++ card/js/config/qrcodeSetup.js
@@ -28,7 +28,7 @@ $("#text").
 
     
 function makeCode () {      
-    
+
     var elText = "00020101021215314182058600049992010210742100010520407425303586540530.005802PK5915Test Merchant 56008Karachi 626001202020061518020230093005202020061518020230093007088913100163040d79";
     
     // if (!elText.value) {
@@ -39,3 +39,30 @@ function makeCode () {
     
     qrcode.makeCode(elText);
 }
+
+
+const getQRcodeImage = async () => {
+    try {        
+        let canvas= await qrcode.toCanvas(`00020101021215314182058600049992010210742100010520407425303586540530.005802PK5915Test Merchant 56008Karachi 626001202020061518020230093005202020061518020230093007088913100163040d79`);
+
+        //adding a log at center        
+        const imgDim={width:30,height:30}; //logo dimention
+        var context = canvas.getContext('2d');
+        var imageObj = new Image();  
+        imageObj.src = '../../../img/unionpay.png';        
+        imageObj.onload = function() {
+          context.drawImage(imageObj, 
+          canvas.width / 2 - imgDim.width / 2,
+          canvas.height / 2 - imgDim.height / 2,imgDim.width,imgDim.height);
+        };        
+        return canvas;
+    }catch (e) {
+        console.error(e);
+        return "";
+    }
+}
+
+
+$(document).ready(function () {
+    getQRcodeImage().then(res => console.log(res));
+})
