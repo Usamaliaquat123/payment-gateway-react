0e94b6e8496453ee046d5fe6042b57c6ed5e1acd
Usama Liaquat <usama.liaquat@paysyslabs.com>
2020-06-25 16:12:06 +0500

    a

--- merchantapp/src/containers/auth/Signup/Signup.js
+++ merchantapp/src/containers/auth/Signup/Signup.js
@@ -3,68 +3,86 @@ import React, { Component } from 'react';
 // import { Button } from 'react-bootstrap';
 import './Signup.css';
 // import { Button } from 'evergreen-ui'
-// import ReactLoading from 'react-loading';
+import ReactLoading from 'react-loading';
+// import { ReactLoading } from 'react-loading';
 
 
 class Signup extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-
+            loading: false
         }
     }
 
 
     componentDidMount() {
-        
+
+    }
+
+    signup() {
+        this.setState({ loading: true})
+        setTimeout(() => {
+            
+            this.setState({ loading: false})
+        }, 5000);
     }
 
 
 
-
-
-    render() {  
+    render() {
+        const {loading} = this.state
         return (
             <div className="container">
-                    <div className="brand_logo">
-                        <p>Paysyslabs</p>
-                    </div>
-                    <p className="acNameText">Account Login</p>
+                <div className="brand_logo">
+                    <p>Paysyslabs</p>
+                </div>
+                <p className="acNameText">Account Login</p>
                 <div className="container_box">
-                    <div className="emailpassaddr">
-                    <p className="lblEP">Email Address</p>
-                    <input
-                    className="inp"
-  name="text-input-name"
-  placeholder="john.doe@paysyslabs.com"
-/>
+
+                    {(loading == false && (
+                        <div>
+
+<div className="emailpassaddr">
+                        <p className="lblEP">Email Address</p>
+                        <input
+                            className="inp"
+                            name="text-input-name"
+                            placeholder="john.doe@paysyslabs.com"
+                        />
                     </div>
                     <div className="emailpassaddr">
-                    <p className="lblEP">Password</p>
-                    <input
-                    className="inp"
-  name="text-input-name"
-  placeholder="********"
-/>
+                        <p className="lblEP">Password</p>
+                        <input
+                            className="inp"
+                            name="text-input-name"
+                            placeholder="********"
+                        />
                     </div>
                     <div className="loginOtherLinks">
                         <div className="row">
                             <div className="col-sm-6">
-                            <div className="textTrouble">
-                            <p>Trouble Logging in ?</p>
-                        </div>
+                                <div className="textTrouble">
+                                    <p>Trouble Logging in ?</p>
+                                </div>
                             </div>
                             <div className="col-sm-6">
-                            <div className="btn_signup">
-                            <p>SIGN UP</p>
-                        </div>
+                                <div className="btn_signup" onClick={() => this.signup()}>
+                                    <p>SIGN UP</p>
+                                </div>
                             </div>
                         </div>
-                        
-                       
-                    </div>
+
+
+                    </div>                            
+                        </div>
+                    ))}
+                    {(loading == true && (
+                        <ReactLoading className="loadingC" type={'bubbles'} color={'#1E2A45'} height={85} width={85} />
+                    ))}
                 </div>
-            </div>            
+
+            </div>
         );
     }
 }
